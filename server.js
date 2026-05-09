f356c1e36ce6d269a258852ee29aa5307d0e277c
Manishasonii <138222595+Manishasonii@users.noreply.github.com>
2026-05-09 23:19:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.use("/api", require("./routes/auth"));
+
+app.listen(process.env.PORT, () => {
+
+  console.log("Server Running");
+});
